91d3ac69c2b78a754b4b99c19f485c2013360cde
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-11-15 07:48:22 +0530

    final upfdate

--- index.js
+++ index.js
@@ -10,24 +10,7 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
-
-// Allow requests from multiple origins
-const corsOptions = {
-  origin: ['https://hashgo.vercel.app', 'http://127.0.0.1:5500'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200 // For legacy browser support
-};
-
-// Apply CORS for all routes with specified options
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
-
-// Additional headers setup (optional if not required by the API)
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // Routes
 app.use('/api/v1/users', userRoutes);
