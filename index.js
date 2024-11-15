ace93c8f75746fe33e9a5164dfe8a38c4b3b5f99
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-11-15 08:02:24 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://hashgo-react.vercel.app', // Replace with your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // If you're using cookies or other credentials
+  })
+);
 
 // Routes
 app.use('/api/v1/users', userRoutes);
