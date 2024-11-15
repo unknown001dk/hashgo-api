f07681ce864d1df9f6b199939f1e39e43636eb47
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-11-15 20:14:01 +0530

    changed mongo

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URL);
-  mongoose.connection.on('connected', () => {
-    console.log('MongoDB connected...');
-  });
-  mongoose.connection.on('error', (err) => {
-    console.error(`MongoDB connection error: ${err.message}`);
-    process.exit(1);
-  });
-}
+  mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to the database'))
+    .catch((error) => console.error('Database connection failed:', error));
+};
